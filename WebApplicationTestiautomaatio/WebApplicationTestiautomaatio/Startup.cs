7df89db72b2f808d5d0aa03a82ef3abfe070553e
Merikjo <johanna.merikanto@stud.pointcollege.fi>
2016-04-26 10:23:16 +0300

    Lisätty uusi testiautomaatio harjoitus käyttäen ASP.NET MVC.

---
+++ WebApplicationTestiautomaatio/WebApplicationTestiautomaatio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationTestiautomaatio.Startup))]
+namespace WebApplicationTestiautomaatio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
